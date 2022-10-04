63819145a85e318a0a747e881d4b3c35c264fb70
Ely <fedotorres41@gmail.com>
2022-10-04 16:43:06 -0500

    "Pruebas de componentes"

--- src/main.js
+++ src/main.js
@@ -3,4 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//Components
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import ScrollPanel from 'primevue/scrollpanel';
+
+// Themes and CSS
+import 'primevue/resources/themes/vela-green/theme.css';
+import 'primevue/resources/primevue.min.css';                 //core css
+import 'primeicons/primeicons.css';  
+
+//Libraries
+import '/node_modules/primeflex/primeflex.css';
+
+const app = createApp(App).use(store).use(router)
+app.use(PrimeVue);
+app.component('OurButton', Button)
+app.component('OurScrollPanel', ScrollPanel)
+
+//Mounting
+app.mount('#app')
+
+
+
